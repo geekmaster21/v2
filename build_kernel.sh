1f0beb1c26f7f2c35f50972efdbe68832425acba
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-02-24 02:20:11 -0500

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,8 +7,13 @@ mkdir out
 BUILD_CROSS_COMPILE=aarch64-elf-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
+KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+export GCC64_DIR=/workspace/v2/gcc-arm64
 
+PATH=$GCC64_DIR/bin/:/usr/bin:$PATH \
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE sm7150_sec_a71_eur_open_defconfig
+
+PATH=$GCC64_DIR/bin/:/usr/bin:$PATH \
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE AR=aarch64-elf-ar OBJDUMP=aarch64-elf-objdump STRIP=aarch64-elf-strip
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image

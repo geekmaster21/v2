9a8b8413ddc00649d8e828dea37106bb4ebaab2c
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-02-24 07:09:28 -0500

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,9 @@ export ARCH=arm64
 mkdir out
 
 BUILD_CROSS_COMPILE=aarch64-elf-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+KERNEL_MAKE_ENV="DTC_EXT=$BUILD_KERNEL_DIR/tools/dtc"
+KERNEL_MAKE_ENV+=" DTC_OVERLAY_TEST_EXT=$BUILD_KERNEL_DIR/tools/ufdt_apply_overlay"
+KERNEL_MAKE_ENV+=" CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
 export GCC64_DIR=/workspace/v2/gcc-arm64
@@ -14,6 +16,6 @@ PATH=$GCC64_DIR/bin/:/usr/bin:$PATH \
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE sm7150_sec_a71_eur_open_defconfig
 
 PATH=$GCC64_DIR/bin/:/usr/bin:$PATH \
-make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE AR=aarch64-elf-ar OBJDUMP=aarch64-elf-objdump STRIP=aarch64-elf-strip
+make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LD=ld.lld AR=aarch64-elf-ar OBJDUMP=aarch64-elf-objdump STRIP=aarch64-elf-strip
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
